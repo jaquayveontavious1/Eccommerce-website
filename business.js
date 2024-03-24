f3efe1e33b9e55d00c28d71626ac1d1df8d7da7a
jaquayveontavious1 <Kanyingitiffany@gmail.com>
2024-03-24 19:50:12 +0300

    added JS

--- business.js
+++ business.js
@@ -1,12 +1,12 @@
 //CART
 let cartIcon = document.getElementById("logo");
 let cart = document.querySelector(".cart");
-let closeCart = document.querySelector(".close-cart");
+let closeCart = document.querySelector("#close-cart");
 //Open Cart
 cartIcon.onclick = () => {
     cart.classList.add("active")
 };
 //Close Cart
-cartIcon.onclick = () => {
+closeCart.onclick = () => {
     cart.classList.remove("active")
 };
