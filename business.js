abc6fc65afc11dc3123bcac593cf58c144902a0c
jaquayveontavious1 <Kanyingitiffany@gmail.com>
2024-03-24 21:16:58 +0300

    added functions to JS

--- business.js
+++ business.js
@@ -10,3 +10,18 @@ cartIcon.onclick = () => {
 closeCart.onclick = () => {
     cart.classList.remove("active")
 };
+
+
+
+//Making Functions
+//Remove Items from the cart
+function remove() {
+    let removeCartButtons = document.getElementsByClassName("cart-remove");
+    console.log(removeCartButtons);
+    for(let i = 0; i < removeCartButtons.length; i++) {
+        
+
+    }
+
+
+}
